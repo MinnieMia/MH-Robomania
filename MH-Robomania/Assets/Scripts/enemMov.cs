7f4e98d0c2ecc62b3e7fe519ee82bd7e2699126e
JuggerNutJoe <130806226+MinnieMia@users.noreply.github.com>
2024-06-21 17:14:15 -0500

    Done

--- MH-Robomania/Assets/Scripts/enemMov.cs
+++ MH-Robomania/Assets/Scripts/enemMov.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class enemMov : MonoBehaviour
 {
     public float yForce;
+    public float xForce;
+    public float xDirection;
     private Rigidbody2D enemyRigidBody;
 
     void Start()
@@ -17,17 +19,23 @@ public class enemMov : MonoBehaviour
         {
             if (transform.position.x <= -8)
             {
-                
+              xDirection = 1;
+              enemyRigidBody.AddForce(Vector2.right*xForce);  
             }
              if (transform.position.x >= 8)
             {
-               
+               xDirection = -1;
+               enemyRigidBody.AddForce(Vector2.left * xForce);
             }
                  
         }
       private void OnCollisionEnter2D(Collision2D collision)
         {
-            
+            if(collision.gameObject.tag == "Ground")
+            {
+                Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+                enemyRigidBody.AddForce(jumpForce);
+            }
         }
     
     
